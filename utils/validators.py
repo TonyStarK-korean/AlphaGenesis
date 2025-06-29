a979bbb8b42bd5d91fa1d594286d828018c2c6de
TonyStarK-korean <outerwoolf@gmail.com>
2025-06-29 13:18:05 +0900

    Auto sync: 2025-06-29 13:18:04

--- utils/validators.py
+++ utils/validators.py
@@ -1,1 +1,18 @@
- 
+class Validators:
+    @staticmethod
+    def is_valid_dataframe(df, required_columns=None):
+        if required_columns is None:
+            required_columns = []
+        if df is None:
+            return False
+        for col in required_columns:
+            if col not in df.columns:
+                return False
+        return True
+
+    @staticmethod
+    def is_positive_number(value):
+        try:
+            return float(value) > 0
+        except Exception:
+            return False 
