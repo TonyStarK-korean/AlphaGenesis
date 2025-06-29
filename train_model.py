699b99ae1269c9b12a7b0863e7f8a4627f7a7313
TonyStarK-korean <outerwoolf@gmail.com>
2025-06-29 16:52:09 +0900

    Auto sync: 2025-06-29 16:52:09

--- train_model.py
+++ train_model.py
@@ -1,8 +1,8 @@
 from ml.models.price_prediction_model import PricePredictionModel
 import pandas as pd
 
-# (예시) 훈련 데이터 로드 - 실제 사용 데이터 경로로 수정하세요!
-train_data = pd.read_csv('data/your_train_data.csv')
+# 샘플 훈련 데이터 로드
+train_data = pd.read_csv('data/market_data/sample_train_data.csv')
 
 # 모델 훈련
 model = PricePredictionModel()
