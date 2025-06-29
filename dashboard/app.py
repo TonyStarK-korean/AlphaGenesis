f1c62fabc18ef214d49927c1fe9782fd7f5d9ca7
TonyStarK-korean <outerwoolf@gmail.com>
2025-06-29 15:19:08 +0900

    Auto sync: 2025-06-29 15:19:07

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src'))
 from flask import Flask, render_template, jsonify, request, send_from_directory
 from flask_cors import CORS
 import pandas as pd
